adf84f8a8625293530f7523fcb08537a94db29b8
Alfred Chan <alfred8844@gmail.com>
2018-07-30 23:25:11 -0400

    fixed breadth-first search

--- src/graph/breadthFirstTraversal.ts
+++ src/graph/breadthFirstTraversal.ts
@@ -7,29 +7,27 @@ import { toIt } from "../iter";
  * @param key turns a node into a unique value, needed if the neighbours
  *  are derived values instead of pointing to the actual nodes in the graph, then a key function must be provided.
  */
-export function* bfsPreOrder<T>(
+export function bfsPreOrder<T>(
     root: T,
-    neighbours: (vertex: T) => IterableIterator<T>,
+    neighbours: (vertex: T) => Iterable<T>,
 ) {
     return toIt(bfsPreOrderHelper(root, neighbours));
 }
 
 function* bfsPreOrderHelper<T>(
     root: T,
-    neighbours: (vertex: T) => IterableIterator<T>,
-): IterableIterator<[T, number]> {
+    neighbours: (vertex: T) => Iterable<T>,
+): Iterable<[T, number]> {
     const workList: Array<[T, number]> = [[root, 0]];
     const visited = new Set<T>();
-
     while (workList.length > 0) {
         const [cur, depth] = workList.shift()!;
         yield [cur, depth];
         visited.add(cur);
         const depth1 = depth + 1;
-
         for (const nei of neighbours(cur)) {
-            visited.add(nei);
             if (!visited.has(nei)) {
+                visited.add(nei);
                 workList.push([nei, depth1]);
             }
         }
